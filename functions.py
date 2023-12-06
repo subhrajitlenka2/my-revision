32deddf5427303dc88fa8c117a654819ea3822ad
subhrajit <subhrajitlenka22254@gmail.com>
2023-12-06 12:13:21 +0530

    deleted unwanted files

--- functions.py
+++ functions.py
@@ -112,17 +112,17 @@
 # modifylist(lst)
 # print("outside function:", lst)
 
-# def factorial(n):
-#     ans = 1
-#     if n == 0:
-#         ans = 1
-#     else:
-#         for i in range(1, n+1):
-#             ans *= i
-#     return ans
-# n = int(input("enter n:"))
-# output = factorial(n)
-# print("the factorial is:", output)
+def factorial(n):
+    ans = 1
+    if n == 0:
+        ans = 1
+    else:
+        for i in range(1, n+1):
+            ans *= i
+    return ans
+n = int(input("enter n:"))
+output = factorial(n)
+print("the factorial is:", output)
 
 
 
